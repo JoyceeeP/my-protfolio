d8ae1ec6bca0817fe42c8362a2c60cde5dae94eb
JoyceeeP <qjppeneira@tip.edu.ph>
2025-01-31 16:50:12 +0800

    updated navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,11 +5,11 @@ const Navbar = () => {
     <nav navclassName='bg-black text-white px-8 md:px-16 lg:px-24'>
         <div className='container py-2 flex justify-center md:justify-between items-center'>
             <div>
-                <div>Joyce</div>
-                <div>
-                    <a href="#home">Home</a>
-                    <a href="#about">About</a>
-                    <a href="#contact">Contact</a>
+                <div className='text-2xl font-bold hidden md:inline'>Joyce</div>
+                <div className='space-x-6'>
+                    <a href="#home" className='hover:text-gray-400'>Home</a>
+                    <a href="#about" className='hover:text-gray-400'>About</a>
+                    <a href="#contact" className='hover:text-gray-400'>Contact</a>
                 </div>
             </div>
         </div>
